68ecab5e35cde61cfe739d2919e13f12152e4309
Hasan EROL <hasanerol89@gmail.com>
2024-11-17 05:49:39 +0300

    Update a.js

--- a.js
+++ a.js
@@ -1,16 +1,1 @@
-var data = null;
 
-var xhr = new XMLHttpRequest();
-xhr.withCredentials = true;
-
-xhr.addEventListener("readystatechange", function () {
-  if (this.readyState === this.DONE) {
-    console.log(this.responseText);
-  }
-});
-
-xhr.open("GET", "https://api.collectapi.com/watching/moviesPlaying");
-xhr.setRequestHeader("content-type", "application/json");
-xhr.setRequestHeader("authorization", "apikey your_token");
-
-xhr.send(data);
